3ecc556dff80e991053494c994a8f949ad9aa9fe
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-10-12 19:49:31 +0300

    update3

--- elite_project/src/component/Home/index.tsx
+++ elite_project/src/component/Home/index.tsx
@@ -1,10 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
+import "../../Css/Home.css";
+import logo from "../../assets/logos/logo1.png";
 
-const Home=()=>{
-    return(
+const Home = () => {
+    return (
         <>
-            <h1>Welcome to Home page</h1>
+            <div className="media-header-container">
+                <div className="media-info">
+                    <span className="badge">A MEDIA SERVICES</span>
+                    <h1 className="title">
+                        Elite Media Houses <br />
+                        <span className="sub-title">Made By <span className="highlight">Elites  For  Elite</span></span>
+                    </h1>
+                    <p className="description">
+                        Welcome To The Home Of Creativity, Uniqueness, Freshness, Originality And More.
+                    </p>
+                </div>
+                <div className="logo-container">
+                    <img src={logo} alt="EMH Logo" className="emh-logo" />
+                </div>
+            </div>
         </>
     );
 }
