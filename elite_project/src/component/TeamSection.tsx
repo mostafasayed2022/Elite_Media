f70400349cfa0a18edda5ca27aec944cdc26807c
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-10-14 14:48:51 +0300

    update

--- elite_project/src/component/TeamSection.tsx
+++ elite_project/src/component/TeamSection.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import "../Css/About.css";
+
+import image1 from "../assets/about/emam.png";
+import image2 from "../assets/about/marawan 1.png";
+import image3 from "../assets/about/hosny.png";
+import image4 from "../assets/about/ali.png";
+import image5 from "../assets/about/el qersh 5.png";
+import image6 from "../assets/about/ahmed 1.png";
+import image7 from "../assets/about/Rana 1 (1).png";
+import image8 from "../assets/about/arwa 1.png";
+import image9 from "../assets/about/nouran 1.png";
+import image10 from "../assets/about/team.png";
+
+
+
 
 
 type TeamMember = {
@@ -10,15 +25,17 @@ type TeamMember = {
 
 
 const teamMembers: TeamMember[] = [
-    { name: 'Mohamed Emam', title: 'Executive Co-Founder', image: '/path/to/image1.jpg' },
-    { name: 'Mariam Basion', title: 'Executive Co-Founder', image: '/path/to/image2.jpg' },
-    { name: 'Mohammed Hosary', title: 'Executive Co-Founder', image: '/path/to/image3.jpg' },
-    { name: 'Amr Ali', title: 'Executive Co-Founder', image: '/path/to/image4.jpg' },
-    { name: 'Yahia A. El Qarsh', title: 'Senior Video Editor', image: '/path/to/image5.jpg' },
-    { name: 'Ahmed Soliman', title: 'Senior Graphic Designer', image: '/path/to/image6.jpg' },
-    { name: 'Rana Mohamed', title: 'HR', image: '/path/to/image7.jpg' },
-    { name: 'Arwa Sakr', title: 'Motion Designer', image: '/path/to/image8.jpg' },
-    { name: 'Nouran Abdeen', title: 'Sales Representative', image: '/path/to/image9.jpg' },
+    { name: 'Mohamed Emam', title: 'Executive Co-Founder', image: `${image1}` },
+    { name: 'Mariam Basion', title: 'Executive Co-Founder', image: `${image2}` },
+    { name: 'Mohammed Hosary', title: 'Executive Co-Founder', image: `${image3}` },
+    { name: 'Amr Ali', title: 'Executive Co-Founder', image: `${image4}` },
+    { name: 'Yahia A. El Qarsh', title: 'Senior Video Editor', image: `${image5}` },
+    { name: 'Ahmed Soliman', title: 'Senior Graphic Designer', image: `${image6}` },
+    { name: 'Rana Mohamed', title: 'HR', image: `${image7}` },
+    { name: 'Arwa Sakr', title: 'Motion Designer', image: `${image8}` },
+    { name: 'Nouran Abdeen', title: 'Sales Representative', image: `${image9}` },
+    { name: '', title: '', image: `${image10}` }
+
 ];
 
 
@@ -29,6 +46,8 @@ const TeamSection: React.FC = () => {
             <div className="team-section">
                 <h2 className="team-title">OUR TEAM</h2>
                 <p className="team-subtitle">Meet the faces behind the magic!</p>
+                <p className="team-subtitle">A Collective of Creative Visionaries and Strategic Thinkers Driving our Success. !</p>
+
                 <div className="team-grid">
                     {teamMembers.map((member, index) => (
                         <div key={index} className="team-card">
@@ -36,12 +55,16 @@ const TeamSection: React.FC = () => {
                             <div className="team-info">
                                 <h3>{member.name}</h3>
                                 <p>{member.title}</p>
+
                             </div>
+
                         </div>
                     ))}
+
+
                 </div>
                 <div className="join-team">
-                    <a href="/join-team" className="join-team-button">JOIN OUR TEAM</a>
+                    <a href="/join" className="join-team-button">JOIN OUR TEAM</a>
                 </div>
             </div>
 
