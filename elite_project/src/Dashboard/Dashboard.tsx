b1657b4199f292dbc9a02073d72726dc324d867a
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-10-15 23:48:10 +0300

    update

--- elite_project/src/Dashboard/Dashboard.tsx
+++ elite_project/src/Dashboard/Dashboard.tsx
@@ -1,15 +1,15 @@
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeDashboard from "./HomeDashboard";
-import AboutDashboard from "./AboutDashboard";
-import WorkDashboard from "./WorkDashboard";
-import ContactDashboard from "./ContactDashboard";
+// import AboutDashboard from "./AboutDashboard";
+// import WorkDashboard from "./WorkDashboard";
+// import ContactDashboard from "./ContactDashboard";
 const Dashboard = () => {
     return (
         <>
             <HomeDashboard/>
-            <AboutDashboard/>
+            {/* <AboutDashboard/>
             <WorkDashboard/>
-            <ContactDashboard/>
+            <ContactDashboard/> */}
         </>
     );
 }
